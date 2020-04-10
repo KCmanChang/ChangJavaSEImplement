27797039a1403d22ccd696e3406043cd97926618
Chang <Chang@Chang-PC>
2020-04-10 19:29:46 +0800

    2020041002

---
+++ ChangJavaSEImplement/src/tw/kc/chang/apps/javase/implement/Implement04JOptionPane.java
@@ -0,0 +1,18 @@
+package tw.kc.chang.apps.javase.implement;
+
+import javax.swing.JOptionPane;
+
+public class Implement04JOptionPane {
+
+	public static void main(String[] args) {
+		String x = JOptionPane.showInputDialog("Input a number");
+		String y = JOptionPane.showInputDialog("Input a number");
+		
+		int intX = Integer.parseInt(x);
+		int intY = Integer.parseInt(y);
+		int intR = intX + intY;
+		JOptionPane.showMessageDialog(null, intX + " + " + intY + " = " + intR);
+
+	}
+
+}
