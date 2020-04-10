c3f651155a7cb413c7bc30beb31e6457ed765e29
Chang <Chang@Chang-PC>
2020-04-10 20:32:02 +0800

    2020041003

---
+++ ChangJavaSEImplement/src/tw/kc/chang/apps/javase/implement/Implement07IfElse.java
@@ -0,0 +1,25 @@
+package tw.kc.chang.apps.javase.implement;
+
+public class Implement07IfElse {
+
+	public static void main(String[] args) {
+		double rand = Math.random()*100+1;
+		System.out.println(rand);
+		
+		int score = (int)rand;
+		// int score = (int)(Math.random()100+1);
+		System.out.println(score);
+		
+		if(score >=90) {
+			System.out.println("A");
+		}else if(score >=80) {
+			System.out.println("B");
+		}else if(score >=70) {
+			System.out.println("C");
+		}else if(score >=60) {
+			System.out.println("D");
+		}else {
+			System.out.println("E");
+		}	
+	}
+}
