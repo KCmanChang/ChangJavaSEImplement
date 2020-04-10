a25434f2aa9bc743d84e73ac4eb77c9f16d1f291
Chang <Chang@Chang-PC>
2020-04-10 15:32:04 +0800

    2020041001

--- ChangJavaSEImplement/src/tw/kc/chang/apps/javase/implement/Implement02Arithmetic.java
+++ ChangJavaSEImplement/src/tw/kc/chang/apps/javase/implement/Implement02Arithmetic.java
@@ -1,9 +1,22 @@
 package tw.kc.chang.apps.javase.implement;
 
+import java.util.Scanner;
+
 public class Implement02Arithmetic {
 
 	public static void main(String[] args) {
-		
+		Scanner scanner = new Scanner(System.in);
+		int x = scanner.nextInt();
+		int y = scanner.nextInt();
+		int resultplus = x + y;
+		int resultminus = x - y;
+		int resultmult = x * y;
+		int resultdivision = x / y;
+		int resultremaind = x % y;
+		System.out.printf("%d + %d = %d\n", x, y, resultplus);
+		System.out.printf("%d - %d = %d\n", x, y, resultminus);
+		System.out.printf("%d * %d = %d\n", x, y, resultmult);
+		System.out.printf("%d / %d = %d......%d\n", x, y, resultdivision, resultremaind);
 	}
 
 }
